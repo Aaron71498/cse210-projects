88c46be243623c8a5700116885693d621182905f
unknown <day16007@byui.edu>
2024-01-11 13:20:11 -0700

    Completed C# Prep 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // asking for first and last name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // returning the names acquired
+        Console.WriteLine("");
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
