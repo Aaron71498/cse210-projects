0ab28bf4256270d1f2150a27765e44394d32ba30
Aaron Day <day16007@byui.edu>
2024-01-20 14:55:40 -0700

    Completed C# Prep 5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // function to welcome the user
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        // function to get the user's name
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        // function to get a number from the user
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        // function to square the user's number
+        static int SquareNumber(int number)
+        {
+            int squareNumber = number * number;
+            return squareNumber;
+        }
+
+        // function to print results
+        static void DisplayResult(string name, int squareNumber)
+        {
+            Console.WriteLine($"{name}, the square of your  number is {squareNumber}.");
+        }
+
+        // use the functions
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squareNumber = SquareNumber(number);
+        DisplayResult(name, squareNumber);
+
     }
 }
