7a3d0b715992c98c7ce656668e6e3dfc60406041
unknown <day16007@byui.edu>
2024-01-30 13:42:36 -0700

    Journal project in progress

---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class PromptGenerator
+{
+    // list of journal prompts
+    public List<string> _prompts = new List<string>
+            {"What was the best thing that happened today?",
+            "What was the worst thing that happened today?",
+            "What was the most interesting thing I saw or heard today?",
+            "What was the most challenging thing I faced today?",
+            "What was the most fun thing I did today?",
+            "What was the most surprising thing that happened today?",
+            "What did I do today that I am proud of?"};
+    
+    // display randomly selected prompt
+    public void Display()
+    {
+        // get and display random prompt
+        Random R = new Random();
+        int number = R.Next(0, _prompts.Count());
+        Console.WriteLine(_prompts.ElementAt(number));
+    }
+}
